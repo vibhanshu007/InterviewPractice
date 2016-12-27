38067af80b803c23d152111c903d0a92722c65fc
vibs <vibhanshumail849@gmail.com>
2016-12-27 15:01:25 +0530

    intersection

--- src/com/vibs/conseptOfday/IntersectionOfArrays.java
+++ src/com/vibs/conseptOfday/IntersectionOfArrays.java
@@ -6,32 +6,51 @@ import java.util.Set;
 
 public class IntersectionOfArrays {
 	
-	static void intersection(Integer[]... intputArray){
-		for (Integer[] integer : intputArray) {
-			System.out.println(Arrays.toString(integer));
-		}
-		Set<Integer> set = new HashSet<Integer>(Arrays.asList(intputArray[0]));
-		
-		
-		
-		for (int i = 0; i < intputArray.length; i++) {
-			Set<Integer> intersection = new HashSet<Integer>(Arrays.asList(intputArray[i]));
-			intersection.retainAll(intersection);
-		}
-		System.out.println(set);
-		
-	}
-	
-	public static void main(String[] args) {
-		 Integer[] inputArray1 = {2, 3, 4, 7, 1};
+	static void intersection(Integer[] ... inputArrays)
+    {    
+        //Printing input arrays
          
-	        Integer[] inputArray2 = {4, 1, 3, 5};
-	         
-	        Integer[] inputArray3 = {8, 4, 6, 2, 1};
-	         
-	        Integer[] inputArray4 = {7, 9, 4, 1};
-	         
-	        intersection(inputArray1, inputArray2, inputArray3, inputArray4);
-	}
-
+        System.out.println("Input Arrays :");
+         
+        System.out.println("======================");
+         
+        for (Integer[] inputArray : inputArrays) 
+        {
+            System.out.println(Arrays.toString(inputArray));
+        }
+         
+        //Creating HashSet object for first input array
+         
+        HashSet<Integer> intersectionSet = new HashSet<>(Arrays.asList(inputArrays[0]));
+         
+        //Calling retainAll() method of first object by passing 2nd, 3rd, 4th... objects
+         
+        for (int i = 1; i < inputArrays.length; i++) 
+        {
+            HashSet<Integer> set = new HashSet<>(Arrays.asList(inputArrays[i]));
+             
+            intersectionSet.retainAll(set);
+        }
+         
+        System.out.println("===========================");
+         
+        System.out.println("Intersection Of All Input Arrays :");
+         
+        System.out.println("===========================");
+         
+        System.out.println(intersectionSet);
+    }
+     
+    public static void main(String[] args)
+    {    
+        Integer[] inputArray1 = {2, 3, 4, 7, 1};
+         
+        Integer[] inputArray2 = {4, 1, 3, 5};
+         
+        Integer[] inputArray3 = {8, 4, 6, 2, 1};
+         
+        Integer[] inputArray4 = {7, 9, 4, 1};
+         
+        intersection(inputArray1, inputArray2, inputArray3, inputArray4);
+    }
 }
